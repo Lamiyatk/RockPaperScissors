6d6d3ce091f963f3bfa365e3ef0f0ad47944ecbc
lamiyatk <lamiya@bititude.com>
2024-08-14 09:55:23 +0530

    update playRound function with switch statement

---
+++ script.js
@@ -0,0 +1,17 @@
+let word = prompt("your choice");
+function getComputerChoice(){
+    const sign = ["rock","paper","scissors"];
+    const randomindex = Math.floor(Math.random() * 3);
+    return sign[randomindex];
+}
+console.log(getComputerChoice());
+
+   
+function getHumanChoice() {
+    if (choice === "paper","rock","scissors");
+}
+console.log("rock");
+console.log("paper");
+console.log("scissors")
+
+
